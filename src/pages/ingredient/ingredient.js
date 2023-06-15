1ba9286072976137c0c1e6477b8e4a097d759e2b
MychalWood <116838101+MychalWood@users.noreply.github.com>
2023-06-14 23:22:25 -0400

    Update ingredient.js

--- src/pages/ingredient/ingredient.js
+++ src/pages/ingredient/ingredient.js
@@ -1,5 +1,24 @@
+import {settings} from "../../config/config";
+import {NavLink, Outlet, useLocation} from "react-router-dom";
+import {useState, useEffect} from "react";
+import useXmlHttp from "../../services/useXmlHttp";
+import {useAuth} from "../../services/useAuth";
 import MenuStyles from "../styles/menu.module.css";
 const Ingredient = () => {
+    const {user} = useAuth();
+    const {pathname} = useLocation();
+    const [subHeading, setSubHeading] = useState("All Ingredients");
+    const url = settings.baseApiUrl + "/ingredient";
+
+    const {
+        error,
+        isLoading,
+        data: ingredient
+    } = useXmlHttp(url, "GET", {Authorization:`Bearer ${user.jwt}`});
+
+    useEffect(() => {
+        setSubHeading("All Ingredients");
+    }, [pathname]);
     return (
        <>
            <div className="main-heading">
