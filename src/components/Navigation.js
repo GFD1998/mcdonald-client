59be6d2fb6cd80f97ea91e8dfc31c010882d0ba6
Gabriel Dennett <50601660+GFD1998@users.noreply.github.com>
2023-06-14 23:25:35 -0400

    Commits

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import NavStyles from '../pages/styles/nav.module.css';
+import Signin from "../pages/auth/signin";
+import Signout from "../pages/auth/signout";
+import Signup from "../pages/auth/signup";
+
+import {settings} from "../config/config";
+import useXmlHttp from '../services/useXmlHttp';
+import {useParams} from "react-router-dom";
+// import './class.css';
+import {useAuth} from "../services/useAuth";
 
 const Navigation = () => {
+
+  
   return (
     <>
       <nav>
@@ -10,6 +21,11 @@ const Navigation = () => {
             <NavLink to="/allergen" className={NavStyles.navLinks}>ALLERGENS</NavLink>
             <NavLink to="/ingredient" className={NavStyles.navLinks}>INGREDIENTS</NavLink>
             <NavLink to="/nutritionalinformation" className={NavStyles.navLinks}>NUTRITIONAL INFORMATION</NavLink>
+            <div className="nav-separator">|</div>
+              {useAuth.isAuthed
+                  ? <NavLink to="/signout">Sign out</NavLink>
+                  : <NavLink to="/signin">Sign in/Sign up</NavLink>
+              }
       </nav>
     </>
   );
