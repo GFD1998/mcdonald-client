7925b334493a22e6c4cb7a498c5d8f90b74ccf19
MychalWood <116838101+MychalWood@users.noreply.github.com>
2023-06-14 23:26:36 -0400

    Update routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -7,10 +7,34 @@ import Allergen from "../pages/allergen/allergen";
 import Ingredient from "../pages/ingredient/ingredient";
 import NutritionalInformation from "../pages/nutritionalInformation/nutritionalinformation";
 
+import {AuthProvider} from "../services/useAuth";
 import Signin from "../pages/auth/signin";
 import Signout from "../pages/auth/signout";
 import Signup from "../pages/auth/signup";
+import RequireAuth from "../components/RequireAuth";
 
+const AppRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home/>}/>
+					<Route path="menuitem" element={<MenuItem/>}/>
+                    <Route path="allergen" element={<Allergen/>}/>
+                    <Route path="ingredient" element={<Ingredient/>}/>
+                    <Route path="nutritionalinformation" element={<NutritionalInformation/>}/>
+                    <Route path="/signin" element={<Signin/>}/>
+                    <Route path="/signout" element={<Signout/>}/>
+                    <Route path="/signup" element={<Signup/>}/>
+                    <Route path="*" element={<NoMatch/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter> 
+        
+    );
+};
+
+export default AppRoutes;
 const AppRoutes = () => {
     return (
         <BrowserRouter>
