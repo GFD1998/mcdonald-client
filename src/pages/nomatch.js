f227a403adc3090eefec790abba399632dd1c028
MychalWood <116838101+MychalWood@users.noreply.github.com>
2023-06-14 23:24:26 -0400

    Update nomatch.js
    Added the files contents of nomatch.js since its contents were blank.

---
+++ src/pages/nomatch.js
@@ -0,0 +1,17 @@
+const NoMatch = () => {
+    return (
+        <>
+            <div className="main-heading">
+                <div className="container">Error</div>
+            </div>
+            <div className="sub-heading">
+                <div className="container">404 Page Not Found</div>
+            </div>
+            <div className="main-content container">
+                The page requested could not found. Please check your request and  try again.
+            </div>
+        </>
+    );
+};
+
+export default NoMatch;
