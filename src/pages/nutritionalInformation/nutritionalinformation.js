a6962507e2f6350538f49f1e470ea6534b479578
MychalWood <116838101+MychalWood@users.noreply.github.com>
2023-06-14 23:23:53 -0400

    Update nutritionalinformation.js

--- src/pages/nutritionalInformation/nutritionalinformation.js
+++ src/pages/nutritionalInformation/nutritionalinformation.js
@@ -1,5 +1,24 @@
+import {settings} from "../../config/config";
+import {NavLink, Outlet, useLocation} from "react-router-dom";
+import {useState, useEffect} from "react";
+import useXmlHttp from "../../services/useXmlHttp";
+import {useAuth} from "../../services/useAuth";
 import MenuStyles from "../styles/menu.module.css";
 const NutritionalInformation = () => {
+    const {user} = useAuth();
+    const {pathname} = useLocation();
+    const [subHeading, setSubHeading] = useState("All Nutritional Information");
+    const url = settings.baseApiUrl + "/nutritionalinformation";
+
+    const {
+        error,
+        isLoading,
+        data: nutritionalinformation
+    } = useXmlHttp(url, "GET", {Authorization:`Bearer ${user.jwt}`});
+
+    useEffect(() => {
+        setSubHeading("All Nutritional Information");
+    }, [pathname]);
     return (
        <>
            <div className="main-heading">
