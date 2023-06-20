1fb96a2fd81a6bfaaf3263ab42de0d489bf2b389
jjtuffnell <97687386+jjtuffnell@users.noreply.github.com>
2023-06-20 17:06:03 -0400

    bug fixes on sign in/up
    fixed Auth in the header, fixed duplication in routes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,13 @@ import FooterStyles from '../pages/styles/footer.module.css';
 
 
 const Footer = () => {
+    const year = new Date().getFullYear(); //gets current year
     return (
-        <>
             <footer>
-                <div>
-                    <span>&copy;McDonald's Corporation.</span>
+                <div className={"container"}>
+                    <span>&copy;McDonald's Corporation. 1955-{year}</span>
                 </div>
             </footer>
-        </>
     );
 };
 
--- src/config/routes.js
+++ src/config/routes.js
@@ -6,7 +6,6 @@ import MenuItem from "../pages/menuitem/menuitem";
 import Allergen from "../pages/allergen/allergen";
 import Ingredient from "../pages/ingredient/ingredient";
 import NutritionalInformation from "../pages/nutritionalInformation/nutritionalinformation";
-
 import {AuthProvider} from "../services/useAuth";
 import Signin from "../pages/auth/signin";
 import Signout from "../pages/auth/signout";
@@ -16,43 +15,26 @@ import RequireAuth from "../components/RequireAuth";
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home/>}/>
-					<Route path="menuitem" element={<MenuItem/>}/>
-                    <Route path="allergen" element={<Allergen/>}/>
-                    <Route path="ingredient" element={<Ingredient/>}/>
-                    <Route path="nutritionalinformation" element={<NutritionalInformation/>}/>
-                    <Route path="/signin" element={<Signin/>}/>
-                    <Route path="/signout" element={<Signout/>}/>
-                    <Route path="/signup" element={<Signup/>}/>
-                    <Route path="*" element={<NoMatch/>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter> 
-        
-    );
-};
-
-export default AppRoutes;
-const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home/>}/>
-					<Route path="menuitem" element={<MenuItem/>}/>
-                    <Route path="allergen" element={<Allergen/>}/>
-                    <Route path="ingredient" element={<Ingredient/>}/>
-                    <Route path="nutritionalinformation" element={<NutritionalInformation/>}/>
-                    <Route path="/signin" element={<Signin/>}/>
-                    <Route path="/signout" element={<Signout/>}/>
-                    <Route path="/signup" element={<Signup/>}/>
-                    {/* <Route path="*" element={<NoMatch/>}/> */}
-                </Route>
-            </Routes>
-        </BrowserRouter> 
-        
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home/>}/>
+                        <Route path="menuitem" element={
+                            <RequireAuth>
+                                <MenuItem/>
+                            </RequireAuth>
+                        }/>
+                        <Route path="allergen" element={<Allergen/>}/>
+                        <Route path="ingredient" element={<Ingredient/>}/>
+                        <Route path="nutritionalinformation" element={<NutritionalInformation/>}/>
+                        <Route path="/signin" element={<Signin/>}/>
+                        <Route path="/signout" element={<Signout/>}/>
+                        <Route path="/signup" element={<Signup/>}/>
+                        <Route path="*" element={<NoMatch/>}/>
+                    </Route>
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
     );
 };
 
